2ff4724d0ac23ae0156e30997d299ce366bd58e7
Andrei <andreiuttzzumarian00@gmail.com>
2020-05-25 13:58:20 +0100

    prima

---
+++ Server.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace tcpServer
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bStartServer = new System.Windows.Forms.Button();
+            this.console = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bStartServer
+            // 
+            this.bStartServer.Location = new System.Drawing.Point(572, 37);
+            this.bStartServer.Name = "bStartServer";
+            this.bStartServer.Size = new System.Drawing.Size(216, 77);
+            this.bStartServer.TabIndex = 0;
+            this.bStartServer.Text = "Start Server";
+            this.bStartServer.UseVisualStyleBackColor = true;
+            this.bStartServer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // console
+            // 
+            this.console.Location = new System.Drawing.Point(35, 37);
+            this.console.Multiline = true;
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(404, 373);
+            this.console.TabIndex = 1;
+            this.console.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.bStartServer);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bStartServer;
+        private System.Windows.Forms.TextBox console;
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tcpServer
+{
+    public partial class Server : Form
+    {
+        void StartServer()
+        {
+            
+            int i = 0;
+        }
+
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartServer();
+            console.AppendText("hello world");
+            console.AppendText(Environment.NewLine);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
